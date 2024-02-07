38472ec230ec05afb44481d3358accbffe137c93
JCCo-Pilot <111530455+JCCo-Pilot@users.noreply.github.com>
2024-02-07 11:01:56 -0600

    start panel imports

--- src/Panels/StartPanel.java
+++ src/Panels/StartPanel.java
@@ -2,6 +2,14 @@ package Panels;
 import java.util.*;
 import javax.swing.*;
 import EventAndListener.*;
+import java.awt.*;
+import java.awt.event.*;
 public class StartPanel extends JPanel{
+    private GameListener listener;
+    public void paint(Graphics g){
 
+    }
+    public void setListener(GameListener gl){
+        listener = gl;
+    }
 }
