1a9ed46fb076c234b6e8ee4e1d24bb8cd90cd2d4
JCCo-Pilot <111530455+JCCo-Pilot@users.noreply.github.com>
2024-02-18 23:54:09 -0600

    filling up the points

--- src/MathHelper/PointGenerator.java
+++ src/MathHelper/PointGenerator.java
@@ -1,11 +1,18 @@
 package MathHelper;
 import java.util.*;
 public class PointGenerator {
-    public PointGenerator(int x, int y, Double size){
+    private MathPoint[] points = new MathPoint[6];
+    public PointGenerator(int x, int y, Double size){//default point generator 0/180 degree
         Double b = ((4.0 - Math.sqrt(7)) / 6) * size;
         Integer sz = (int) Math.round(size);
         Integer a = (int) Math.round(b);
         Integer[] xPoints = {x, x + sz, x + sz, x, x, x};
         Integer[] yPoints = {y, y + a, y + sz - a, y + sz, y + sz - a, y + a};
+        fillPoints(xPoints, yPoints);
+    }
+    private void fillPoints(Integer[] xp, Integer[] yp){
+        for (int i =0; i<6;i++){
+            points[i]= new MathPoint(xp[i], yp[i]);
+        }
     }
 }
