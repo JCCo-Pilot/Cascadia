7527e606e06a4d846a148b6c286e40579b9b92e2
JCCo-Pilot <111530455+JCCo-Pilot@users.noreply.github.com>
2024-02-18 23:45:33 -0600

    point class

---
+++ src/MathHelper/MathPoint.java
@@ -0,0 +1,11 @@
+package MathHelper;
+
+public class MathPoint {
+    public int xPoint;
+    public int yPoint;
+    public MathPoint(int x, int y){
+        xPoint = x;
+        yPoint = y;
+    }
+    
+}
