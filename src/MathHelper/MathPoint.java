dae1a74cfedf01a45a2591f94124535aa6d944b1
aditya-khambekar <150048125+aditya-khambekar@users.noreply.github.com>
2024-02-22 22:16:50 -0600

    Added distance methods to MathPoint

--- src/MathHelper/MathPoint.java
+++ src/MathHelper/MathPoint.java
@@ -7,5 +7,54 @@ public class MathPoint {
         xPoint = x;
         yPoint = y;
     }
+
+    public Double distanceTo(MathPoint p){
+        int x1 = p.xPoint;
+        int y1 = p.yPoint;
+        int x2 = this.xPoint;
+        int y2 = this.yPoint;
+
+        Integer a = x1-x2;
+        Integer b = y1-y2;
+
+        return Math.sqrt(Math.pow(a.doubleValue(), 2)+Math.pow(b.doubleValue(), 2));
+
+    }
+
+    public Double distanceTo(int x, int y){
+        int x1 = x;
+        int y1 = y;
+        int x2 = this.xPoint;
+        int y2 = this.yPoint;
+
+        Integer a = x1-x2;
+        Integer b = y1-y2;
+
+        return Math.sqrt(Math.pow(a.doubleValue(), 2)+Math.pow(b.doubleValue(), 2));
+
+    }
+
+    public static Double distanceBetween(int x1, int y1, int x2, int y2){
+        
+
+        Integer a = x1-x2;
+        Integer b = y1-y2;
+
+        return Math.sqrt(Math.pow(a.doubleValue(), 2)+Math.pow(b.doubleValue(), 2));
+
+    }
+
+    public static Double distanceBetween(MathPoint point1, MathPoint point2){
+        int x1 = point1.xPoint;
+        int y1 = point1.yPoint;
+        int x2 = point2.xPoint;
+        int y2 = point2.yPoint;
+
+        Integer a = x1-x2;
+        Integer b = y1-y2;
+
+        return Math.sqrt(Math.pow(a.doubleValue(), 2)+Math.pow(b.doubleValue(), 2));
+
+    }
     
 }
