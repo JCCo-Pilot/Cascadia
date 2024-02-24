4d1017159d78d7f37d188de1c0d11df87a86dcc3
aditya-khambekar <150048125+aditya-khambekar@users.noreply.github.com>
2024-02-24 16:51:09 -0600

    player has graph

--- src/Entities/Player.java
+++ src/Entities/Player.java
@@ -4,10 +4,14 @@ public class Player {
     private int natureTokens;
     private int turns;
     private String name;
+    private HabitatGraph graph;
     public Player(int i){
         name= "Player "+i;
         turns = 20;
     }
     public void setName(String s){name = s;}
     public String getName(){return name;}
+    public HabitatGraph getGraph(){
+        return graph;
+    }
 }
