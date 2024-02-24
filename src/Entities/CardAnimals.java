19080185f148385cf864d473af2c5db867b78d80
aditya-khambekar <150048125+aditya-khambekar@users.noreply.github.com>
2024-02-24 16:40:49 -0600

    what even is an enum

---
+++ src/Entities/CardAnimals.java
@@ -0,0 +1,26 @@
+package Entities;
+
+public enum CardAnimals {
+    BEAR, 
+    ELK,
+    SALMON,
+    HAWK,
+    FOX;
+
+    public static CardAnimals StringToAnimal(String s){
+        switch(s.toLowerCase()){
+            case "bear":
+                return BEAR;
+            case "elk":
+                return ELK;
+            case "salmon":
+                return SALMON;
+            case "hawk":
+                return HAWK;
+            case "fox":
+                return FOX;
+            default:
+                return null;   
+        }
+    }
+}
