d881d7895f39a6961b9dfbc2ad2ac760132b9b30
JCCo-Pilot <111530455+JCCo-Pilot@users.noreply.github.com>
2024-02-25 12:29:52 -0600

    events time

--- src/EventAndListener/AllowPickEvent.java
+++ src/EventAndListener/AllowPickEvent.java
@@ -1,5 +1,12 @@
 package EventAndListener;
-
-public class AllowPickEvent {
-
+import java.util.*;
+public class AllowPickEvent extends EventObject{
+    private boolean allow;
+    public AllowPickEvent(Object source,boolean b){
+        super(source);
+        allow = b;
+    }
+    public boolean allowed(){
+        return allow;
+    }
 }
