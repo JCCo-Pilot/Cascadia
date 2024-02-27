f8be036147650b173fae00c29f4fb69bdda3adc0
JCCo-Pilot <111530455+JCCo-Pilot@users.noreply.github.com>
2024-02-26 22:22:37 -0600

    yup

--- src/Entities/HabitatTiles.java
+++ src/Entities/HabitatTiles.java
@@ -280,7 +280,7 @@ public class HabitatTiles extends PointGenerator{
 
     public boolean canPick(WildlifeTokens tk){
         out.println("canPick method:"+tk.toString());
-        if (token==null){
+        if (token==null&&tk!=null){
             /*if(animals.contains(CardAnimals.StringToAnimal(tk.getName()))){
                 System.out.println("Called");
                 return true;
