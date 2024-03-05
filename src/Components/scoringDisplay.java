7262e46757dc54c80604ed066d27cd80d99b0bc8
JCCo-Pilot <111530455+JCCo-Pilot@users.noreply.github.com>
2024-03-05 07:07:49 -0600

    scoring display

--- src/Components/scoringDisplay.java
+++ src/Components/scoringDisplay.java
@@ -12,10 +12,23 @@ public class scoringDisplay extends JComponent implements ActionListener{
     private int numPlayers;
     private int xPos,yPos;
     private int xSize,ySize;
+    private ArrayList<JButton>playerButtons = new ArrayList<>();
     public scoringDisplay(HashMap<Integer,Player>play , int num, int x, int y, int xs, int ys){
         numPlayers = num;
         players = play;
+        xPos = x; yPos = y;
+        xSize = xs; ySize = ys;
     }
+    public void paintComponents(Graphics g){
+        g.drawRect(0,0,100,100);
+    }
+    public int getXPos(){return xPos;}
+    public int getYPos(){return yPos;}
+    public int getXSize(){return xSize;}
+    public int getYSize(){return ySize;}
+    public Dimension getPreferredSize(){return new Dimension(xSize,ySize);}
+    public Dimension getMinimuSize(){return getPreferredSize();}
+    public Dimension getMaximumSize(){return getPreferredSize();}
     public void paint(Graphics g){
         g.setColor(Color.CYAN);
         
--- src/Panels/EndPanel.java
+++ src/Panels/EndPanel.java
@@ -1,20 +1,26 @@
 package Panels;
 import java.util.*;
 import javax.swing.*;
+
+import Components.scoringDisplay;
 import EventAndListener.*;
 import java.awt.event.*;
 import java.awt.*;
 public class EndPanel extends JPanel implements ActionListener,MouseListener{
+    private scoringDisplay sd;
     public EndPanel(){
         setLayout(null);
+        sd = new scoringDisplay(null, HEIGHT, FRAMEBITS, ERROR, ALLBITS, ABORT)
         repaint();
     }
     public void paint(Graphics g){
-        g.fillRect(0, 0, 1590, 865);
+        //g.fillRect(0, 0, 1590, 865);
         paintComponents(g);
     }
     public void actionPerformed(ActionEvent e){}
-    public void mouseClicked(MouseEvent e) {}
+    public void mouseClicked(MouseEvent e) {
+        repaint();
+    }
     public void mousePressed(MouseEvent e) {}
     public void mouseReleased(MouseEvent e) {}    
     public void mouseEntered(MouseEvent e) {}
