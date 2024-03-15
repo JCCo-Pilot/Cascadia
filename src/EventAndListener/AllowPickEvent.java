5bc2a449e632895c752062e20e41d9eb6d9f1b55
JCCo-Pilot <111530455+JCCo-Pilot@users.noreply.github.com>
2024-03-15 16:31:04 -0500

    null handling

--- src/EventAndListener/AllowPickEvent.java
+++ src/EventAndListener/AllowPickEvent.java
@@ -1,12 +1,23 @@
 package EventAndListener;
 import java.util.*;
+import Entities.*;
 public class AllowPickEvent extends EventObject{
     private boolean allow;
+    private Player movedOn;
     public AllowPickEvent(Object source,boolean b){
         super(source);
         allow = b;
+        movedOn = null;
+    }
+    public AllowPickEvent(Object source, Player p){
+        super(source);
+        movedOn = p;
+        allow = false;
     }
     public boolean allowed(){
         return allow;
+    }
+    public Player movedOn(){
+        return movedOn;
     }
 }
