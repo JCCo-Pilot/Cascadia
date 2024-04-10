33572b06e47c0beea2e82d2091c2a1d8b2b9521c
JCCo-Pilot <111530455+JCCo-Pilot@users.noreply.github.com>
2024-04-09 19:50:17 -0500

    scoring card pop up

--- src/Components/SelectedScoringCard.java
+++ src/Components/SelectedScoringCard.java
@@ -8,7 +8,28 @@ import java.awt.event.*;
 import javax.imageio.ImageIO;
 import java.awt.image.*;
 import MathHelper.*;
+import Entities.WildlifeScoringCards.*;
 import static java.lang.System.*;
 public class SelectedScoringCard extends JFrame{
     private JPanel panel;
+    public SelectedScoringCard(){
+        super("Scoring Card Pop Up");
+        this.setSize(500,500);
+    }
+
+    public void addScoringCard(BearCard bc){
+
+    }
+    public void addScoringCard(ElkCard ec){
+
+    }
+    public void addScoringCard(FoxCard fc){
+
+    }
+    public void addScoringCard(HawkCard hc){
+
+    }
+    public void addScoringCard(SalmonCard sc){
+        
+    }
 }
