e326c7e26dc5a6cf03f2eba7818b1ba7525b1449
JCCo-Pilot <111530455+JCCo-Pilot@users.noreply.github.com>
2024-04-09 19:40:01 -0500

    scoring card pop up

---
+++ src/Components/SelectedScoringCard.java
@@ -0,0 +1,14 @@
+package Components;
+import Entities.*;
+import java.util.*;
+import javax.swing.*;
+import EventAndListener.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.imageio.ImageIO;
+import java.awt.image.*;
+import MathHelper.*;
+import static java.lang.System.*;
+public class SelectedScoringCard extends JFrame{
+    private JPanel panel;
+}
