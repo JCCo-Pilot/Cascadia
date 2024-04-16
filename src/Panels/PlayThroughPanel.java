ab0342cd5651d2c99c5d4739789629b4c87001ec
JCCo-Pilot <111530455+JCCo-Pilot@users.noreply.github.com>
2024-04-16 16:35:41 -0500

    working on it

--- src/Panels/PlayThroughPanel.java
+++ src/Panels/PlayThroughPanel.java
@@ -13,16 +13,28 @@ import Components.*;
 import Entities.WildlifeScoringCards.*;
 import Entities.Enums.*;
 import static java.lang.System.*;
-public class PlayThroughPanel extends JPanel implements MouseListener,ActionListener,EndGameListener{
+public class PlayThroughPanel extends JPanel implements MouseListener,ActionListener,EndGameListener,AllowPickEventListener{
     private BufferedImage background;
     private PlayerDisplay pd;
-    private PickArea pa;
+    public ArrayList<HabitatTiles>habitatTiles = new ArrayList<>();
+    public HabitatTiles[]hexagons = new HabitatTiles[4];
     private ArrayList<Player>players = new ArrayList<>();
     private ArrayList<JButton>buttons = new ArrayList<>();
+
+    private int state;
     public PlayThroughPanel(){
         setLayout(null);
 
+        state = 1;
+
         players.add(new Player(1));
+        habitatTiles= new TileCreator().getTiles();
+        for (int i = 0;i<4;i++){
+            hexagons[i]= habitatTiles.remove(0);
+            hexagons[i].setX(56+69);
+            hexagons[i].setY(275+(146*i)-100);
+        }
+
         constructStarters();
         try{
             background = ImageIO.read(new File("src/Panels/Background/MainPanelBG.png"));
@@ -45,7 +57,7 @@ public class PlayThroughPanel extends JPanel implements MouseListener,ActionList
         pd.setBounds(pd.getXPos(),pd.getYPos(),pd.getPreferredSize().width,pd.getPreferredSize().height);
         add(pd);
 
-        
+        pd.addListener(this);
 
         this.setVisible(true);
     }
@@ -111,6 +123,22 @@ public class PlayThroughPanel extends JPanel implements MouseListener,ActionList
         if(background!=null){
             g.drawImage(background, 0, 0, 1590,865,null);
         }
+        //start painting stuff here
+        switch(state){
+            case 1: //choosing a habitat tile
+                g.setColor(new Color(255, 255, 143));
+                g.fillRect(56+69-70,175-80,140,600);
+            break;
+            case 2: //choosing a nature token
+            break;
+            case 3://wait for player to place
+            break;
+        }
+        
+        for(int i =0;i<4;i++){
+            hexagons[i].drawHexagon(g);
+        }
+        
         paintComponents(g);
     }
 
@@ -158,6 +186,12 @@ public class PlayThroughPanel extends JPanel implements MouseListener,ActionList
     public void endGameTime(EndGameEvent e) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'endGameTime'");
+    }
+
+    @Override
+    public void process(AllowPickEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'process'");
     }
     
 }
