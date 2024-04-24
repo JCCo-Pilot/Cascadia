5054bf22304aaa39947a001fae180bd115f8832e
K1341584 <111533520+K1341584@users.noreply.github.com>
2024-04-24 08:53:45 -0500

    Update PopPanel.java

--- src/Panels/PopPanel.java
+++ src/Panels/PopPanel.java
@@ -1,5 +1,10 @@
 package Panels;
 import Entities.*;
+import Entities.WildlifeScoringCards.BearCard;
+import Entities.WildlifeScoringCards.ElkCard;
+import Entities.WildlifeScoringCards.FoxCard;
+import Entities.WildlifeScoringCards.HawkCard;
+import Entities.WildlifeScoringCards.SalmonCard;
 import Entities.WildlifeScoringCards.ScoringCard;
 
 import java.util.*;
@@ -22,6 +27,11 @@ public class PopPanel extends JComponent implements MouseListener, ActionListene
     private GameListener listener;
     private BufferedImage bg;
     private BufferedImage bg2;
+    private BearCard bearCard;
+    private ElkCard elkCard;
+    private SalmonCard salmonCard;
+    private HawkCard hawkCard;
+    private FoxCard foxCard;
     public PopPanel(int a){
         super();
         this.setVisible(true);
@@ -83,14 +93,23 @@ public class PopPanel extends JComponent implements MouseListener, ActionListene
             g.drawString(""+p.getNatureTokens(),950,375+73+300);
             //Total Score
             g.drawString(""+p.getScore(),945,375+77+360);
+            paintCards(g);
             paintComponents(g);
         }
         else if(state == 2) {
         	g.drawImage(bg2, 0, 0, getWidth(), getHeight(), null);
             g.setColor(Color.BLACK);
             back.setBounds(1050,790,150,50);
+            paintCards(g);
         	paintComponents(g);
         }
+    }
+    public void paintCards(Graphics g){
+        g.drawImage(bearCard.getImage(),1020,10,250,230,null);
+        g.drawImage(elkCard.getImage(),1300,10,250,230,null);
+        g.drawImage(salmonCard.getImage(), 1020, 240, 250, 230, null);
+        g.drawImage(hawkCard.getImage(), 1300, 240, 250, 230, null);
+        g.drawImage(foxCard.getImage(), 1020, 470, 250, 230, null);
     }
     public void pullImages(){
         try{
@@ -121,6 +140,22 @@ public class PopPanel extends JComponent implements MouseListener, ActionListene
         this.setVisible(false);
         GameStateEvent gse = new  GameStateEvent(back, 50);
         listener.process(gse);
+    }
+  //setter methods
+    public void setBearCard(BearCard bc){
+        bearCard = bc;
+    }
+    public void setElkCard(ElkCard ec){
+        elkCard = ec;
+    }
+    public void setSalmonCard(SalmonCard sc){
+        salmonCard = sc;
+    }
+    public void setHawkCard(HawkCard hc){
+        hawkCard = hc;
+    }
+    public void setFoxCard(FoxCard fc){
+        foxCard = fc;
     }
     public void mouseClicked(MouseEvent e) {}
     public void mousePressed(MouseEvent e) {
