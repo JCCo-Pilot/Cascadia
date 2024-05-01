b3b9b56b5964e03d8b9ad79b2a352a5cb5564a76
JCCo-Pilot <111530455+JCCo-Pilot@users.noreply.github.com>
2024-05-01 10:09:04 -0500

    fix

--- src/Panels/MainPanel.java
+++ src/Panels/MainPanel.java
@@ -294,12 +294,13 @@ public class MainPanel extends JPanel implements MouseListener,ActionListener,En
         sTiles.add(new StarterTile(tiles).setPos(438, 456, 70.0));
         tiles.clear();
         //tile 2
-        tiles.add(new HabitatTiles("forest-elk-key", new String[]{"forest"}, new String[]{"elk"}, true, 250, 100, size,true));
+        //fix this here/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /*tiles.add(new HabitatTiles("forest-elk-key", new String[]{"forest"}, new String[]{"elk"}, true, 250, 100, size,true));
         tiles.add(new HabitatTiles("lake+mountain-hawk-elk-bear", new String[]{"lake","mountain"}, new String[]{"bear","elk","hawk"}, false, 190, 205, size,true));//240
         tiles.get(1).setOffset(240);
         tiles.add(new HabitatTiles("desert+swamp-fox-salmon", new String[]{"desert","swamp"}, new String[]{"fox","salmon"}, false, 310, 205, size,true));//300
         tiles.get(2).setOffset(300);
-        sTiles.add(new StarterTile(tiles).setPos(438, 456, 70.0));
+        sTiles.add(new StarterTile(tiles).setPos(438, 456, 70.0));*/
         tiles.clear();
         //tile 3
         tiles.add(new HabitatTiles("forest-elk-key", new String[]{"forest"}, new String[]{"elk"}, true, 250, 100, size,true));
